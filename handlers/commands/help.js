d96dd230d5af5fe7c1fcaba345fd0c7b4bfa6080
Mihir Kumar Singh <mihir39nr@gmail.com>
2024-01-17 01:17:08 +0530

    Modified Commands

--- handlers/commands/help.js
+++ handlers/commands/help.js
@@ -14,8 +14,7 @@ If you want to use me for your groups, \
 note that I'm more useful on a network of groups and \
 you also need to <b>setup a new bot.</b>
 
-So if you don't wish to self-host, @MissRose_bot \
-might be a better choice for you.
+
 `;
 
 /** @param { import('../../typings/context').ExtendedContext } ctx */
@@ -24,9 +23,9 @@ const helpHandler = (ctx) => {
 
 	return ctx.replyWithHTML(
 		message,
-		Markup.inlineKeyboard([
-			Markup.button.url('🛠 Setup a New Bot', homepage)
-		])
+		// Markup.inlineKeyboard([
+		// 	Markup.button.url('🛠 Setup a New Bot', homepage)
+		// ])
 	);
 };
 
